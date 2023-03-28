180ca53bfb3055e14f7425696d2e714dd774961a
Gecko05 <jacacent@gmail.com>
2023-03-28 17:56:39 -0600

    openai

--- webserver/recipes.py
+++ webserver/recipes.py
@@ -1,54 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
+import os
+import openai
 
-def search_recipes(ingredients, exclude=[], cuisine=''):
-    """
-    This function searches for recipes on Allrecipes.com based on the given ingredient criteria, excluding any
-    recipes that contain any of the excluded ingredients and filtering by the specified cuisine type (if any).
-    """
-    url = 'https://www.allrecipes.com/search/results/?wt={}&sort=re&page={}&pht={}'
-    recipes = []
-    page_number = 1
-    while True:
-        print("asking")
-        res = requests.get(url.format(ingredients, page_number, cuisine))
-        print(res)
-        soup = BeautifulSoup(res.content, 'html.parser')
-        results = soup.find_all('article', {'class': 'fixed-recipe-card'})
-        if not results: # no more results, break out of the loop
-            break
-        print(results)
-        for result in results:
-            recipe = {}
-            recipe['name'] = result.find('h3', {'class': 'fixed-recipe-card__h3'}).text.strip()
-            recipe['rating'] = result.find('span', {'class': 'stars'}).attrs['data-ratingstars']
-            recipe['url'] = result.find('a', {'class': 'fixed-recipe-card__title-link'}).attrs['href']
-            recipe['ingredients'] = result.find('div', {'class': 'fixed-recipe-card__ingredients'}).text.strip()
-            # check if any excluded ingredients are present in the recipe
-            excluded = False
-            for exc in exclude:
-                if exc in recipe['ingredients']:
-                    excluded = True
-                    break
-            # check if the recipe's cuisine type matches the specified cuisine (if any)
-            if cuisine:
-                ct = result.find('span', {'class': 'fixed-recipe-card__category'})
-                if not ct or cuisine.lower() not in ct.text.lower():
-                    continue
-            if not excluded:
-                recipes.append(recipe)
-        page_number += 1
-    return recipes
+openai.api_key = "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"
 
-# example usage:
-ingredients = 'chicken, rice, broccoli'
-exclude = ['onion', 'garlic']
-cuisine = 'chinese'
-print("rPrinting")
-recipes = search_recipes(ingredients, exclude, cuisine)
-for recipe in recipes:
-    print(recipe['name'])
-    print(recipe['rating'])
-    print(recipe['url'])
-    print(recipe['ingredients'])
-    print()
+comletition = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    messages = [
+        {
+            "role": "user", "content": "Tell me recipes with the following ingredients:"
+        }
+    ]
+)
+
+print(comletition.choices[0].messages.content)
